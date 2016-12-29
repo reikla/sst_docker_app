c4488114e0d0991972e1f64aed9b33579f7b3f02
reimarklammer <reimar_klammer@gmx.at>
2016-12-29 18:36:06 +0100

    listening port geaendert

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace CoreWebApp
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseUrls("http://*:5000")
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
